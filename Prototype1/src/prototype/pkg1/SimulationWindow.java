51df051bdfae193e80ac8c689ffa28c9ffd62e84
lakksan10 <48561162+lakksan10@users.noreply.github.com>
2019-04-01 13:11:08 +0100

    Update SimulationWindow.java

--- Prototype1/src/prototype/pkg1/SimulationWindow.java
+++ Prototype1/src/prototype/pkg1/SimulationWindow.java
@@ -1,34 +1,15 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package prototype.pkg1;
+package prototype1.pkg1;
 
-/**
- *
- * @author 8804
- */
 public class SimulationWindow extends GUI{
-    
-    boolean play;
+
+    public SimulationWindow(int width, int height) {
+        super(width, height);
+        super.setTitle("SimulationWindow");
+    }
     
     @Override
     public void show(){
-        primaryStage.show();
-    }
-
-    SimulationWindow(){
-        play = false;
+       super.show();
     }
     
-    public void play(){
-        play = true;
-        System.out.println(play);
-    }
-    
-    public void pause(){
-        play = false;
-        System.out.println(play);
-    }
 }
