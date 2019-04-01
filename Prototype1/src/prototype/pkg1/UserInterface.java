98ae2b12e7e3fec0540d9d7dfed6be10135e6870
lakksan10 <48561162+lakksan10@users.noreply.github.com>
2019-04-01 13:13:14 +0100

    Update and rename ControlBox.java to UserInterface.java

---
+++ Prototype1/src/prototype/pkg1/UserInterface.java
@@ -0,0 +1,16 @@
+
+package prototype1.pkg1;
+
+public class UserInterface extends GUI {
+    
+    public UserInterface(int width, int height) {
+        super(width, height);
+        super.setTitle("UserInterface");
+    }
+    
+    @Override
+    public void show(){
+       super.show();
+    }
+    
+}
